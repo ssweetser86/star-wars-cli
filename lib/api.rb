d22f96b7969f0d3b6f9dadea37917622e447076d
Stephen <stephenjsweets@gmail.com>
2020-12-09 14:48:10 -0500

    having issue with uri and https

--- lib/api.rb
+++ lib/api.rb
@@ -2,17 +2,32 @@ require_relative '../lib/environment.rb'
 
 class API
 
-    URL = "https://swapi.dev/api/people/"
+    #URL = "https://swapi.dev/api/people/"
 
-    def get_data
-        uri = URI.parse(URL)
+    def get_data(url = "https://swapi.dev/api/people/?page=1")
+        uri = URI.parse(url)
+        binding.pry
+        uri.scheme = "https"
         response = Net::HTTP.get_response(uri)
         data = JSON.parse(response.body)
         return data
     end
 
+    def compile_all
+        sw = get_data
+        data = []
+        #binding.pry
+        while sw["next"] != nil
+            sw["results"].each { |v| data << v }
+            sw = get_data(sw["next"])
+        end
+        return data
+
+    end
+
+
 
 end
 
 sw = API.new
-binding.pry
+sw.compile_all
