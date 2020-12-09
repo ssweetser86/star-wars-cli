3eb51d7c644c67fd4819565e42db55fe6535d61e
Stephen <stephenjsweets@gmail.com>
2020-12-09 13:42:22 -0500

    testing scrape methods

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,18 @@
+require_relative '../lib/environment.rb'
+
 class API
 
+    URL = "https://swapi.dev/api/people/"
+
+    def get_data
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        data = JSON.parse(response.body)
+        return data
+    end
+
+
 end
+
+sw = API.new
+binding.pry
