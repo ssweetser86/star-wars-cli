a5152e559b5bee888d6ab90a72e95c4d1502bb04
Stephen <stephenjsweets@gmail.com>
2020-12-10 13:58:50 -0500

    added basic methods and variables

--- lib/character.rb
+++ lib/character.rb
@@ -1,3 +1,18 @@
 class Character
 
+    @@all = []
+
+    attr_accessor :name, :dob, :homeworld
+
+    def initialize(name, dob, homeworld)
+        @name = name
+        @dob = dob
+        @homeworld = homeworld
+        @@all << self
+    end
+
+    def all
+        @@all
+    end
+
 end
--- lib/planet.rb
+++ lib/planet.rb
@@ -1,3 +1,17 @@
 class Planet
 
+    @@all = []
+
+    attr_accessor :name, :population
+
+    def initialize(name, population)
+        @name = name
+        @population = population
+        @@all << self
+    end
+
+    def all
+        @@all
+    end
+
 end
