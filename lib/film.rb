f9df60d70dc1eee002ac828913f6e4753ac3a0ec
Stephen <stephenjsweets@gmail.com>
2020-12-10 13:58:32 -0500

    added basic methods and variables

--- lib/film.rb
+++ lib/film.rb
@@ -1,3 +1,18 @@
 class Film
 
+    @@all = []
+
+    attr_accessor :name, :director, :crawl
+
+    def initialize(name, director, crawl)
+        @name = name
+        @director = director
+        @crawl = crawl
+        @@all << self
+    end
+
+    def all
+        @@all
+    end
+
 end
